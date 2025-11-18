1f301542722db37332795023b8e2f7bcd8564f9d
restaurantreviewcards <jake@restaurantreviewcards.com>
2025-11-18 18:50:51 -0500

    new

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/_includes/**/*.{html,js,njk}", // Now correctly scans layouts
-    "./src/content/**/*.{html,js,njk}",  // Now correctly scans content pages
-    "./src/*.{html,js,njk}"              // Now correctly scans root files inside src (like index.html, if applicable)
+    "./src/_includes/**/*.{html,js,njk}", 
+    "./src/content/**/*.{html,js,njk}",  
+    "./src/*.{html,js,njk}"              
   ],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Inter', 'sans-serif'],
+      },
+      // This centers the container by default and adds padding
+      container: {
+        center: true,
+        padding: '1rem',
+        screens: {
+          sm: '640px',
+          md: '768px',
+          lg: '1024px',
+          xl: '1280px',
+          // We stop growing after 1280px so it doesn't look "zoomed out" on big screens
+          '2xl': '1280px', 
+        },
       },
     },
   },
