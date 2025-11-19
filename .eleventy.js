7150c2fd33ce8160170760773c98ec4cfbe32d7e
restaurantreviewcards <jake@restaurantreviewcards.com>
2025-11-18 19:26:24 -0500

    new

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,13 @@
 module.exports = function(eleventyConfig) {
     
-    // --- 1. Passthrough Copy (FIXED) ---
-    // We use a mapping object { "source_path": "target_path_in_site" }
-    // This tells Eleventy to grab files from 'src/' and put them at the root of the site.
-    
+    // Copy Assets
     eleventyConfig.addPassthroughCopy({ "src/images": "images" });
     eleventyConfig.addPassthroughCopy({ "src/script.js": "script.js" });
     
-    // Tailwind generates the CSS into src/, so we copy it from there to the site root
-    eleventyConfig.addPassthroughCopy({ "src/styles.css": "styles.css" });
+    // Copy our single CSS file
+    eleventyConfig.addPassthroughCopy({ "src/style.css": "style.css" });
 
-    // --- 2. Watch Configuration ---
-    eleventyConfig.addWatchTarget("src/styles.css");
-
-    // --- 3. Absolute URL Filter ---
+    // Absolute URL Filter
     eleventyConfig.addFilter("absoluteUrl", (url, base) => {
         try {
             return new URL(url, base).href;
@@ -24,9 +18,9 @@ module.exports = function(eleventyConfig) {
 
     return {
         dir: {
-            input: "src",       // Source files are here
-            includes: "_includes", 
-            output: "_site"     // Final build is here
+            input: "src",
+            includes: "_includes",
+            output: "_site"
         }
     };
 };
